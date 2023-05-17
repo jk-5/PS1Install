146f34826e83fe261c4c11c051939b016bbcf256
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 18:22:11 +0200

    Update Install6.ps1

--- Install6.ps1
+++ Install6.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
+cls
+
 # Definicja zmiennych
 $klitecodecpackUrl = "https://files3.codecguide.com/K-Lite_Codec_Pack_1760_Mega.exe"
 $installDir = "C:\Program Files (x86)\K-Lite Codec Pack"
