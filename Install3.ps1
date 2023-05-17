20e5104d1d1d4eaad8cb1144811c61f774678994
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 16:35:23 +0200

    Update Install3.ps1

--- Install3.ps1
+++ Install3.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 # Definicja zmiennych
-$irfanviewUrl = "https://www.fosshub.com/IrfanView.html?dwl=iview462_x64_setup.exe"
-$pluginsUrl = "https://www.fosshub.com/IrfanView.html/iview462_plugins_x64_setup.exe"
+$irfanviewUrl = "https://download.instalki.org/programy/Windows/Grafika/przegladarki_grafiki/iview462_x64_setup.exe"
+$pluginsUrl = "http://www.storage.programosy.pl/iview462_plugins_x64_setup.exe"
 $langUrl = "https://www.irfanview.net/lang/irfanview_lang_polski.exe"
 $installDir = "C:\Program Files\IrfanView"
 
