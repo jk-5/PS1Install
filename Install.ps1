132a3851f629f0ad5fc8044e8f29dd0367ddcc93
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 18:21:09 +0200

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
+cls
+
 $file = "C:\Program Files\WinRAR\rarreg.key"
 $app = "C:\Program Files\WinRAR\WinRAR.exe"
 
