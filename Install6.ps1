423001a6c9f8b038782bbc02f712dc0b957264cd
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 18:08:27 +0200

    Add files via upload

---
+++ Install6.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = "Stop"
+# Enable TLSv1.2 for compatibility with older clients
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+# Definicja zmiennych
+$klitecodecpackUrl = "https://files3.codecguide.com/K-Lite_Codec_Pack_1760_Mega.exe"
+$installDir = "C:\Program Files (x86)\K-Lite Codec Pack"
+$app = "C:\Program Files (x86)\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe"
+
+# Pobieranie i instalacja K-Lite Codec Pack Mega
+Write-Host "Instalacja K-Lite Codec Pack Mega."
+$klitecodecpackInstaller = "$env:TEMP\k_lite_codec_pack_setup.exe"
+Invoke-WebRequest -Uri $klitecodecpackUrl -OutFile $klitecodecpackInstaller
+Start-Process -FilePath $klitecodecpackInstaller -Wait
+Remove-Item -Path $klitecodecpackInstaller
+
+# Uruchomienie K-Lite Codec Pack Mega
+cls
+Write-Host "Instalacja K-Lite Codec Pack Mega ukończona."
+Start-Process -FilePath $app
