955eaa500b38ac9644a961c7d63e9e41703f5b3a
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 18:21:46 +0200

    Update Install4.ps1

--- Install4.ps1
+++ Install4.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
+cls
+
 # Definicja zmiennych
 $chromeUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
 $installDir = "C:\Program Files\Google\Chrome"
