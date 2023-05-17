75a0e3f140b66ad103710e6f94ff37c3174d4d76
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 18:21:59 +0200

    Update Install5.ps1

--- Install5.ps1
+++ Install5.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
+cls
+
 # Definicja zmiennych
 $acrordrUrl = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300120064/AcroRdrDC2300120064_pl_PL.exe"
 $installDir = "C:\Program Files\Adobe\Acrobat DC\Acrobat"
