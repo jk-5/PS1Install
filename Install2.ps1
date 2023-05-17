be121366829c1e4f3a9995bfb70ef181cec2f80f
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 18:21:22 +0200

    Update Install2.ps1

--- Install2.ps1
+++ Install2.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
+cls
+
 # Definicja zmiennych
 $winrarUrl = "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-621pl.exe"
 $file = "C:\Program Files\WinRAR\rarreg.key"
