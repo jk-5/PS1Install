4f9c6b58be8dd1dbbca903571ba3e4526fd67216
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 17:48:11 +0200

    Add files via upload

---
+++ Install5.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = "Stop"
+# Enable TLSv1.2 for compatibility with older clients
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+# Definicja zmiennych
+$acrordrUrl = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300120064/AcroRdrDC2300120064_pl_PL.exe"
+$installDir = "C:\Program Files\Adobe\Acrobat DC\Acrobat"
+$app = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
+
+# Pobieranie i instalacja Adobe Acrobat Reader DC
+Write-Host "Instalacja acrordr."
+$acrordrInstaller = "$env:TEMP\acrobat_reader_dc_setup.exe"
+Invoke-WebRequest -Uri $acrordrUrl -OutFile $acrordrInstaller
+Start-Process -FilePath $acrordrInstaller -Wait
+Remove-Item -Path $acrordrInstaller
+
+# Uruchomienie Adobe Acrobat Reader DC
+cls
+Write-Host "Instalacja Adobe Acrobat Reader DC ukończona."
