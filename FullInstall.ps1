3aaed0c1591e40d2d51125071598b682cd9822e4
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-17 18:22:23 +0200

    Update FullInstall.ps1

--- FullInstall.ps1
+++ FullInstall.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
+cls
+
 # Instalacja Chrome
 irm https://raw.githubusercontent.com/jk-5/PS1Install/main/Install4.ps1 | iex
 
