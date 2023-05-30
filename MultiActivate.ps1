5d78af3970c0d09af289351760a8470d10f98f56
640509-040147 <70533838+jk-5@users.noreply.github.com>
2023-05-30 02:35:51 +0200

    Update MultiActivate.ps1

--- MultiActivate.ps1
+++ MultiActivate.ps1
@@ -48,11 +48,7 @@ do {
         }
         2 {
             Write-Host "Aktywacja Office 2019/2021"
-	    $aioUrl = "https://raw.githubusercontent.com/abbodi1406/KMS_VL_ALL_AIO/master/KMS_VL_ALL_AIO.cmd"
-            $aioInstaller = "$env:TEMP\ALL_AIO.cmd"
-	    Invoke-WebRequest -Uri $aioUrl -OutFile $aioInstaller
-	    Start-Process -FilePath $aioInstaller -Wait
-	    Remove-Item -Path $aioInstaller
+	    irm https://raw.githubusercontent.com/jk-5/PS1Install/main/ActivateKMS.ps1 | iex
         }
         0 {
             Write-Host "Zamykanie programu..."
