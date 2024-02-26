4a29b3afb79526cca8cc666f6020e7f022f062ff
640509-040147 <70533838+jk-5@users.noreply.github.com>
2024-02-26 14:02:50 +0100

    Update 6.21

--- Download_CCleaner.ps1
+++ Download_CCleaner.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 
 cls
 
-$ccleanerUrl = "https://download.ccleaner.com/portable/ccsetup619.zip"
+$ccleanerUrl = "https://download.ccleaner.com/portable/ccsetup621.zip"
 
 Write-Host "Pobieranie CCleaner Portable."
 $ccleanerInstaller = "$env:TEMP\ccleaner_portable.zip"
