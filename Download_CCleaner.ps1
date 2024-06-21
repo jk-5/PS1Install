3f471b2f8ad4814f13fd3f2657e298e99553b69a
640509-040147 <70533838+jk-5@users.noreply.github.com>
2024-06-21 12:55:01 +0200

    Update 6.25

--- Download_CCleaner.ps1
+++ Download_CCleaner.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 
 cls
 
-$ccleanerUrl = "https://download.ccleaner.com/portable/ccsetup623.zip"
+$ccleanerUrl = "https://download.ccleaner.com/portable/ccsetup625.zip"
 
 Write-Host "Pobieranie CCleaner Portable."
 $ccleanerInstaller = "$env:TEMP\ccleaner_portable.zip"
