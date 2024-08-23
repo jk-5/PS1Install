5665287d3fe36cc15b190a801ef38973c022c7a7
640509-040147 <70533838+jk-5@users.noreply.github.com>
2024-08-23 14:37:15 +0200

    Update

--- MultiInstall.bat
+++ MultiInstall.bat
@@ -1,2 +1,17 @@
 @echo off
-powershell -Command "irm https://raw.githubusercontent.com/jk-5/PS1Install/main/MultiInstall.ps1 | iex"
+@chcp 852
+:MENU
+cls
+echo ษอออออออออออออออออออออออออออออออออป
+echo บ         Wybierz z listy         บ
+echo ฬอออออออออออออออออออออออออออออออออน
+echo บ 1) Z GUI                        บ
+echo บ 2) Bez GUI                      บ
+echo ศอออออออออออออออออออออออออออออออออผ
+echo.
+set /p choice=Wybierz: 
+
+if "%choice%"=="1" powershell -Command "irm https://raw.githubusercontent.com/jk-5/PS1Install/main/MultiInstall.ps1 | iex"
+if "%choice%"=="2" powershell -Command "irm https://raw.githubusercontent.com/jk-5/PS1Install/main/MultiInstall2.ps1 | iex"
+if "%choice%"=="0" exit
+goto MENU
